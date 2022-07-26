41e1ce1d5a5c12daa1dd11de3b1a03b1f1059a6f
mark-summerfield <mark@qtrac.eu>
2022-07-26 10:33:59 +0100

    upver

--- py/uxf.py
+++ py/uxf.py
@@ -19,7 +19,7 @@ from xml.sax.saxutils import escape, unescape
 import editabletuple
 
 
-__version__ = '2.0.2' # uxf module version
+__version__ = '2.0.3' # uxf module version
 VERSION = 1.0 # UXF file format version
 
 UTF8 = 'utf-8'
