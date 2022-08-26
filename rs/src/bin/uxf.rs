153436f11fb0b8128fd42d1eedb08438f5ef7504
mark-summerfield <mark@qtrac.eu>
2022-08-26 08:44:44 +0100

    started on rs cli

--- rs/src/bin/uxf.rs
+++ rs/src/bin/uxf.rs
@@ -2,8 +2,15 @@
 // License: GPLv3
 
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Args {
+}
 
 fn main() -> Result<()> {
+    let args = Args::parse();
     let uxo = uxf::parse("uxf 1.0\n#<A Test>\n[]")?;
     println!("uxf cli:\n\n{}", uxo);
     Ok(())
