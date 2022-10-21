c03eb7c88e6a1fbe4ff0a7a01b022c4980ca6f65
mark-summerfield <mark@qtrac.eu>
2022-10-21 09:07:10 +0100

    abandoned attempt at C++ version (my C++ is just too rusty)

--- cpp/src/scalarvalue.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright © 2022 Mark Summerfield. All rights reserved.
-// License: GPLv3
-
-#include "scalarvalue.hpp"
-
-namespace uxf {
-
-
-};
--- cpp/src/uxf.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright © 2022 Mark Summerfield. All rights reserved.
-// License: GPLv3
-
-namespace uxf {
-
-
-}
